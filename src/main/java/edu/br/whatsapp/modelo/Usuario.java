71ebc89fff698e21da793da64817e99ae14d368d
Priscylla Silva <mss.priscylla@gmail.com>
2019-07-01 10:29:23 -0300

    Adição da lista de contatos na classe Usuario

--- src/main/java/edu/br/whatsapp/modelo/Usuario.java
+++ src/main/java/edu/br/whatsapp/modelo/Usuario.java
@@ -1,11 +1,14 @@
 package edu.br.whatsapp.modelo;
 
+import java.util.List;
+
 public class Usuario {
 	
 	private String nome;
 	private String foto;
 	private String recado;
 	private String numero;
+	private List<Usuario> contatos;
 	
 	public Usuario(String nome, String numero) {
 		super();
@@ -20,34 +23,44 @@ public class Usuario {
 		// TODO Auto-generated constructor stub
 	}
 
-
-
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getFoto() {
 		return foto;
 	}
+	
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+	
 	public String getRecado() {
 		return recado;
 	}
+	
 	public void setRecado(String recado) {
 		this.recado = recado;
 	}
+	
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-	
-	
-	
+
+	public List<Usuario> getContatos() {
+		return contatos;
+	}
+
+	public void setContatos(List<Usuario> contatos) {
+		this.contatos = contatos;
+	}
 	
 }
